4e0b50972aa05d0a7d37d0b057d526bc68115691
jieun-h5ng <77313847+jieun-h5ng@users.noreply.github.com>
2021-01-14 22:36:23 +0900

    Delete css_js directory

--- css_js/main.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn = document.querySelector('.index_bar');
-const menu = document.querySelector('.index_menu');
-
-toggleBtn.addEventListener('click', () => {
-   menu.classList.toggle('active');
-});
-
-
